546098d8f0a618bcd2c68b3aa615fcc5a7301194
Achmad Iqbal Priranda <iqbalp@webmail.umm.ac.id>
2022-11-22 10:40:56 +0700

    Update Title

--- Assets/Scripts/MovingUI.cs
+++ Assets/Scripts/MovingUI.cs
@@ -5,11 +5,13 @@ using DG.Tweening;
 
 public class MovingUI : MonoBehaviour
 {
+    public RectTransform TitleGame;
     public RectTransform playButton;
     
     // Start is called before the first frame update
     void Start()
     {
+        TitleGame.DOScale(new Vector3(9f, 1.75f, 1f), 2).SetLoops(-1, LoopType.Yoyo);
         playButton.DOScale(new Vector3(1.1f, 1.1f, 1.1f), 2).SetLoops(-1, LoopType.Yoyo);
         
     }
