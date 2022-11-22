527f09b90c8f408b52990e906de45ccdf46b1261
Achmad Iqbal Priranda <iqbalp@webmail.umm.ac.id>
2022-11-22 16:33:48 +0700

    DOTween at obstacle

--- Assets/Scripts/Obstacles/EnemyRed.cs
+++ Assets/Scripts/Obstacles/EnemyRed.cs
@@ -1,38 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class EnemyRed : MonoBehaviour
 {
-    public int speed;
-    bool isMoveUp;
+    public int durationPerLoop;
     
     // Start is called before the first frame update
     void Start()
     {
-        //membuat variasi gerakan bila 2 objek yg sama muncul berdekatan
-        isMoveUp = (Random.Range(0, 2) == 0);
+        transform.DOMoveY(14f, durationPerLoop).SetLoops(-1, LoopType.Yoyo);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y < 12f)
-        {
-            isMoveUp = true;
-        }
-        if(transform.position.y > 14f)
-        {
-            isMoveUp = false;
-        }
-
-        if(isMoveUp)
-        {
-            transform.Translate(Vector3.up * Time.deltaTime * speed);
-        }
-        else
-        {
-            transform.Translate(Vector3.up * Time.deltaTime * -speed);
-        }
+        
     }
 }
